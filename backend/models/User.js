b64ad18eea95f45970f84632dbd1e8d50a7ea4e9
Thuy Vy <thuyvy102@gmail.com>
2025-10-11 21:41:31 +0700

    feat(database): add User model

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+// backend/models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      trim: true,
+      lowercase: true,
+      unique: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
