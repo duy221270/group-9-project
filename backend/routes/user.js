d23c796ef91e897127d80c4be0f180a737872217
duy221270 <duy221270@student.nctu.edu.vn>
2025-10-24 10:14:12 +0700

    feat: Hoàn thành API GET/PUT /profile (Hoạt động 2)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
+// Import middleware
+const { protect } = require('../middleware/authMiddleware');
 
 // Import các hàm xử lý từ controller
 const {
@@ -9,13 +11,21 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  getUserProfile,      // <-- Import hàm mới
+  updateUserProfile, // <-- Import hàm mới
 } = require('../controllers/userController.js');
 
-// Định nghĩa các đường dẫn (routes)
-router.get('/users', getAllUsers);
-router.post('/users', createUser);
-router.put('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)      // GET /api/users/profile
+  .put(protect, updateUserProfile);   // PUT /api/users/profile
+
+// --- CÁC ROUTES CŨ (CHO ADMIN - Hoạt động 3) ---
+// Bạn có thể cần thêm 'protect' và middleware 'admin' ở Hoạt động 3
+router.get('/users', getAllUsers);        // 
+router.post('/users', createUser);      // 
+router.put('/users/:id', updateUser);     // 
+router.delete('/users/:id', deleteUser);  // 
 
 // Chỉ export duy nhất router
 module.exports = router;
