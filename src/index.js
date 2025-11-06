7b9bb0ac600e4282e2b2302a93ebafeae78f7423
trang2903 <nguyenquynhthaotrang290304@gmail.com>
2025-11-07 02:20:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-reportWebVitals();
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -24,4 +11,3 @@ root.render(
     <App />
   </Provider>
 );
-origin/feature/redux-protected
