6a11dc9eec930a04df0b82dcdf252c6d52820b99
trang2903 <nguyenquynhthaotrang290304@gmail.com>
2025-11-07 02:09:47 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,12 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -30,4 +24,4 @@ root.render(
     <App />
   </Provider>
 );
->>>>>>> origin/feature/redux-protected
+origin/feature/redux-protected
